d2185ef975c531b275a2fb5090d19d8b79aa671f
Odunayo <sogungbure@gmail.com>
2022-01-17 13:08:45 +0100

    Write post seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'image_url' => $this->faker->imageUrl(640, 480, 'cats'),
+            'body' => $this->faker->paragraph(3, false),
+        ];
+    }
+}
